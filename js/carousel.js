7d6231631ccd392b8d7b4d526950c14e561c391d
심규장 Gyu Jang Sim <gyujang95@gmail.com>
2020-08-24 17:22:06 +0900

    이미지 부드럽게 넘어가게 하기(수정), 아래 indicator 생성

--- js/carousel.js
+++ js/carousel.js
@@ -1,7 +1,6 @@
 const previous = document.querySelector(".exhibition .btnbox .fa-chevron-left"),
   next = document.querySelector(".exhibition .btnbox .fa-chevron-right"),
   exhibition = document.querySelector(".exhibition"),
-  photo = document.querySelector(".exhibition__photo"),
   btnColorList = ["grey", "#87ceeb", "pink", "#8CE6B6"],
   backgroundColorList = ["black", "blue", "red", "green"],
   exhibitionNameList = ["DESIGN DOERS.", "THE GIFTED", "전시3", "전시4"],
@@ -17,9 +16,28 @@ const previous = document.querySelector(".exhibition .btnbox .fa-chevron-left"),
     "2020.01.12~2020.05.23",
     "2020.05.12~2020.07.23",
   ],
-  imgSrcList = ["imgs/1.jpg", "imgs/2.jpg", "imgs/3.jpg", "imgs/4.jpg"];
-let btn = document.querySelector(".exhibition__info__link"),
+  imgSrcList = ["imgs/1.png", "imgs/2.png", "imgs/3.jpg", "imgs/4.jpg"],
+  btn = document.querySelector(".exhibition__info__link"),
+  indicators = document.querySelector(".exhibition__indicators");
+let photo = document.querySelector(".exhibition__photo"),
   counter = 0;
+
+//indicators
+
+for (let i = 0; i < imgSrcList.length; i++) {
+  const indicator = document.createElement("div");
+  indicator.setAttribute("class", "exhibition__indicator");
+  indicators.appendChild(indicator);
+}
+
+const indicate = (counter) => {
+  //indicator들 중 counter에 해당하는 indicator 길게
+
+  for (let i = 0; i < imgSrcList.length; i++) {
+    indicators.children[i].classList.remove("exhibition__indicator-current");
+  }
+  indicators.children[counter].classList.add("exhibition__indicator-current");
+};
 
 //set
 const set = () => {
@@ -31,57 +49,63 @@ const set = () => {
     exhibitionDiscriptionList[counter];
   exhibition.querySelector(".exhibition__info__date").innerHTML =
     exhibitionDateList[counter];
+  indicate(counter);
 
   btn.addEventListener("mouseenter", () => {
     btn.style.backgroundColor = btnColorList[counter];
-    btn.removeEventListener("mouseenter", arguments.callee);
   });
   btn.addEventListener("mouseleave", () => {
     btn.style.backgroundColor = backgroundColorList[counter];
-    btn.removeEventListener("mouseleave", arguments.callee);
   });
 };
 
 //초기화
 
 set();
+photo.src = imgSrcList[counter];
 
 //클릭이벤트
 
-const fadeout = () => {
-  photo.style.opacity -= 1 / 10;
+const fadeout = (el) => {
+  el.style.opacity -= 1 / 20;
 };
 
 const handlePrevious = (e) => {
-  photo.style.opacity = 1;
-  let fade = window.setInterval(fadeout, 10);
-  setTimeout(() => {
-    clearInterval(fade);
-    photo.style.opacity = 1;
-    photo.src = imgSrcList[counter];
-  }, 400);
-
   counter--;
   if (counter < 0) counter += backgroundColorList.length;
 
-  set();
+  photo = document.querySelector(".exhibition__photo");
+  let photo_tmp = photo.cloneNode(true);
+  photo_tmp.src = imgSrcList[counter];
+  photo_tmp.onload = () => {
+    set();
+    exhibition.insertBefore(photo_tmp, photo);
+    let fade = setInterval(fadeout(photo), 400);
+    setTimeout(clearInterval(fade), 400);
+  };
 };
 
 const handleNext = (e) => {
-  photo.style.opacity = 1;
-  let fade = window.setInterval(fadeout, 10);
-  setTimeout(() => {
-    clearInterval(fade);
-    photo.style.opacity = 1;
-    photo.src = imgSrcList[counter];
-  }, 400);
-
   counter++;
   if (counter >= backgroundColorList.length)
     counter -= backgroundColorList.length;
 
-  set();
+  photo = document.querySelector(".exhibition__photo");
+  let photo_tmp = photo.cloneNode(true);
+  photo_tmp.src = imgSrcList[counter];
+  photo_tmp.onload = () => {
+    set();
+    exhibition.insertBefore(photo_tmp, photo);
+    let fade = setInterval(fadeout(photo), 400);
+    setTimeout(clearInterval(fade), 400);
+  };
 };
 
 previous.addEventListener("click", handlePrevious);
 next.addEventListener("click", handleNext);
+
+let autoCarousel = setInterval(handleNext, 5000);
+
+// 클릭하면 자동으로 넘어가는 기능 사라지게
+// previous.addEventListener("click", () => clearInterval(autoCarousel));
+// next.addEventListener("click", () => clearInterval(autoCarousel));
