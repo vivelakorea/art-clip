4659f2505ecbced6738179f094c8b40128165976
심규장 Gyu Jang Sim <gyujang95@gmail.com>
2020-09-26 14:54:59 +0900

    Update carousel.js
    리스트 여러개를 객체 리스트 하나로 묶음

--- js/carousel.js
+++ js/carousel.js
@@ -4,40 +4,53 @@ const previous = document.querySelector(".exhibition .btnbox .fa-chevron-left"),
   btn = document.querySelector(".exhibition__info__link"),
   link = document.querySelector(".exhibition__info__anchor"),
   indicators = document.querySelector(".exhibition__indicators"),
-  btnColorList = ["grey", "#87ceeb", "pink", "#8CE6B6"],
-  backgroundColorList = ["black", "blue", "red", "green"],
-  exhibitionNameList = ["DESIGN DOERS.", "THE GIFTED", "전시3", "전시4"],
-  exhibitionDiscriptionList = [
-    "'行動하는 디자이너'<br> 디자인의 전통은 '실제(praxis)'를 지향한다.",
-    "대학은 어떤 것의 중요성과 업(業)에 대한 신념에 눈뜨는<br> 인식과 예비의 장으로서 스스로 찾는 지적 축적과 수양을 통해...",
-    "설명3.......",
-    "설명4.......",
-  ],
-  exhibitionDateList = [
-    "2020.03.12~2020.04.23",
-    "2020.01.12~2020.02.23",
-    "2020.01.12~2020.05.23",
-    "2020.05.12~2020.07.23",
-  ],
-  imgSrcList = [
-    "imgs/exhibition/1.jpg",
-    "imgs/exhibition/2.jpg",
-    "imgs/exhibition/3.jpg",
-    "imgs/exhibition/4.jpg",
-  ],
-  exhibitionLinkList = [
-    "http://hongikid.ac.kr/exhibitions/2019",
-    "http://hongikid.ac.kr/exhibitions/2018",
-    "#",
-    "#",
+  exhibitions = [
+    {
+      btnColor: "grey",
+      backgroundColor: "black",
+      name: "DESIGN DORES.",
+      description:
+        "'行動하는 디자이너'<br> 디자인의 전통은 '실제(praxis)'를 지향한다.",
+      date: "2020.03.12~2020.04.23",
+      img: "imgs/exhibition/1.jpg",
+      link: "http://hongikid.ac.kr/exhibitions/2019",
+    },
+    {
+      btnColor: "87ceeb",
+      backgroundColor: "blue",
+      name: "THE GIFTED",
+      description:
+        "대학은 어떤 것의 중요성과 업(業)에 대한 신념에 눈뜨는<br> 인식과 예비의 장으로서 스스로 찾는 지적 축적과 수양을 통해...",
+      date: "2020.01.12~2020.02.23",
+      img: "imgs/exhibition/2.jpg",
+      link: "http://hongikid.ac.kr/exhibitions/2018",
+    },
+    // {
+    //   btnColor: "pink",
+    //   backgroundColor: "red",
+    //   name: "전시 3",
+    //   description: "설명 3....................................................",
+    //   date: "2020.01.12~2020.05.23",
+    //   img: "imgs/exhibition/3.jpg",
+    //   link: "#",
+    // },
+    // {
+    //   btnColor: "#8CE6B6",
+    //   backgroundColor: "green",
+    //   name: "전시 4",
+    //   description: "설명 4....................................................",
+    //   date: "2020.05.12~2020.07.23",
+    //   img: "imgs/exhibition/4.jpg",
+    //   link: "#",
+    // },
   ];
 
 let photo = document.querySelector(".exhibition__photo"),
   counter = 0;
 
-//indicators
+//indicators(carousel 아래 찍히는 점 + 초록색 현재 창)
 
-for (let i = 0; i < imgSrcList.length; i++) {
+for (let i = 0; i < exhibitions.length; i++) {
   const indicator = document.createElement("div");
   indicator.setAttribute("class", "exhibition__indicator");
   indicators.appendChild(indicator);
@@ -46,7 +59,7 @@ for (let i = 0; i < imgSrcList.length; i++) {
 const indicate = (counter) => {
   //indicator들 중 counter에 해당하는 indicator 길게
 
-  for (let i = 0; i < imgSrcList.length; i++) {
+  for (let i = 0; i < exhibitions.length; i++) {
     indicators.children[i].classList.remove("exhibition__indicator-current");
   }
   indicators.children[counter].classList.add("exhibition__indicator-current");
@@ -54,29 +67,29 @@ const indicate = (counter) => {
 
 //set
 const set = () => {
-  exhibition.style.backgroundColor = backgroundColorList[counter];
-  btn.style.backgroundColor = backgroundColorList[counter];
+  exhibition.style.backgroundColor = exhibitions[counter].backgroundColor;
+  btn.style.backgroundColor = exhibitions[counter].backgroundColor;
   exhibition.querySelector(".exhibition__info__name").innerHTML =
-    exhibitionNameList[counter];
+    exhibitions[counter].name;
   exhibition.querySelector(".exhibition__info__discription").innerHTML =
-    exhibitionDiscriptionList[counter];
+    exhibitions[counter].description;
   exhibition.querySelector(".exhibition__info__date").innerHTML =
-    exhibitionDateList[counter];
+    exhibitions[counter].date;
   indicate(counter);
-  link.setAttribute("href", exhibitionLinkList[counter]);
+  link.setAttribute("href", exhibitions[counter].link);
 
   btn.addEventListener("mouseenter", () => {
-    btn.style.backgroundColor = btnColorList[counter];
+    btn.style.backgroundColor = exhibitions[counter].btnColor;
   });
   btn.addEventListener("mouseleave", () => {
-    btn.style.backgroundColor = backgroundColorList[counter];
+    btn.style.backgroundColor = exhibitions[counter].backgroundColor;
   });
 };
 
 //초기화
 
 set();
-photo.src = imgSrcList[counter];
+photo.src = exhibitions[counter].img;
 
 //클릭이벤트
 
@@ -86,11 +99,11 @@ const fadeout = (el) => {
 
 const handlePrevious = (e) => {
   counter--;
-  if (counter < 0) counter += backgroundColorList.length;
+  if (counter < 0) counter += exhibitions.length;
 
   photo = document.querySelector(".exhibition__photo");
   let photo_tmp = photo.cloneNode(true);
-  photo_tmp.src = imgSrcList[counter];
+  photo_tmp.src = exhibitions[counter].img;
   photo_tmp.onload = () => {
     set();
     exhibition.insertBefore(photo_tmp, photo);
@@ -105,12 +118,11 @@ const handlePrevious = (e) => {
 
 const handleNext = (e) => {
   counter++;
-  if (counter >= backgroundColorList.length)
-    counter -= backgroundColorList.length;
+  if (counter >= exhibitions.length) counter -= exhibitions.length;
 
   photo = document.querySelector(".exhibition__photo");
   let photo_tmp = photo.cloneNode(true);
-  photo_tmp.src = imgSrcList[counter];
+  photo_tmp.src = exhibitions[counter].img;
   photo_tmp.onload = () => {
     set();
     exhibition.insertBefore(photo_tmp, photo);
